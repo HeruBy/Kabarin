1637db86ddd2d97786d125530271b988dd792a5b
kuru2501 <34452482+kuru2501@users.noreply.github.com>
2017-12-13 00:18:29 +0700

    Add files via upload
    belum rampung

--- app/src/main/java/com/example/heruby/kabarin/MainActivity.java
+++ app/src/main/java/com/example/heruby/kabarin/MainActivity.java
@@ -1,47 +1,44 @@
-package com.example.heruby.kabarin;
+package com.example.akbarramadhan.kabarin_recycleview;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    RecyclerView recyclerView;
+    AgendaAdapter adapter;
+    LayoutInflater layoutInflater;
+
+    List<Agenda> productList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
-                Boolean isFirstStart = pref.getBoolean("firststart",true);
-                if (isFirstStart){
-                    startActivity(new Intent(MainActivity.this,IntroActivity.class));
-                    SharedPreferences.Editor e = pref.edit();
-                    e.putBoolean("firststart",false);
-                    e.apply();
-                }
-            }
-        });
-        thread.start();
+        productList = new ArrayList<>();
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        layoutInflater = getLayoutInflater();
+
+        FloatingActionButton fabulous = (FloatingActionButton) findViewById(R.id.fab_create_new_item);
+        fabulous.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        int viewId = v.getId();
+        if (viewId == R.id.fab_create_new_item){
+            controller.createNewListItem(); //referensi https://www.youtube.com/watch?v=-xuhicDXMvw
+                                            //tapi masih bingung
+        }
+    }
 }
